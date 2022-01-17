a88ee51723749c26180c291c2a9d14496fe2ea65
fenceMeshwire <94779778+fenceMeshwire@users.noreply.github.com>
2022-01-17 21:07:16 +0100

    Create rotateSpinner.py
    Purpose: Create a moving spinner with the characters = ['|', '/', '-', '\\']

---
+++ rotateSpinner.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+# Python 3.9.5
+
+# rotatingSpinner.py
+
+# Purpose: Create a moving spinner with the characters = ['|', '/', '-', '\\'] 
+
+import random, time
+
+class Rotate():
+    def __init__(self, downloadSize):
+        self.downloadedBytes = 0
+        self.downloadSize = downloadSize
+        self.characters = ['|', '/', '-', '\\']
+
+    def spinner(self):
+        while self.downloadedBytes < self.downloadSize:
+            # Simulation of the download:
+            self.downloadedBytes += random.randint(0,1000)
+
+            for char in self.characters:
+                print('\b\b', char, end='', flush=True)
+                time.sleep(0.05)
+
+        print('\nDownload completed!')
+        
+downloadSize = 8192
+
+oRotate = Rotate(downloadSize)
+oRotate.spinner()
